52177ce9ebc71cef6f679dea76cee24d840f1929
Anurag Verma <anuragverma65@gmail.com>
2023-01-29 09:07:57 +0000

    setup code formatting and gitignore updates
    adds prettier
    stops tracking yarn error log file

--- README.md
+++ README.md
@@ -9,4 +9,3 @@ A simple rest-api built with Node.js
 ## License
 
 This project is licensed under the MIT License - see the LICENSE file for details
-
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response } from "express";
 
-const app = express()
+const app = express();
 
-const PORT = 5000
+const PORT = 5000;
 
-app.get('/', (_req: Request, res: Response) => {
-    return res.send('Hello world!')
-}) 
+app.get("/", (_req: Request, res: Response) => {
+  return res.send("Hello world!");
+});
 
 app.listen(PORT, () => {
-    console.log('Server is running on port 5000')
-})
+  console.log("Server is running on port 5000");
+});
